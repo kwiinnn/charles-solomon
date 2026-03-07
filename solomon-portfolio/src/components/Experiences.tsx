bf2475e6fb15ee3e8a81a5b506f416239b898dc1
kwiinnn <solomon23charles@gmail.com>
2026-03-07 22:29:00 +0800

    refactor: transferred Experiences section to react

---
+++ solomon-portfolio/src/components/Experiences.tsx
@@ -0,0 +1,39 @@
+export function Experiences () {
+  return (
+    <>
+      <section id="experience" className="card fade-in-up">
+        <h2>Experience</h2>
+        <div className="experiences-container">
+          <div className="timeline-item">
+            <div className="experience-title">AI Frontiers / AI MSME Hackathon (2025)</div>
+            <div className="muted experience-role">Frontend Developer</div>
+            <p className="muted experience-description">Built the frontend UI for our hackathon project, creating responsive layouts and real-time interactions. Contributed to our Top 5 finish as the youngest team and the only group of first-year students.</p>
+          </div>
+          <div className="timeline-item">
+            <div className="experience-title">SHS National Research Summit (2025)</div>
+            <div className="muted experience-role">Co-Lead Developer</div>
+            <p className="muted experience-description">Title: Exploring the Utilization of a Mediapipe-based Hand-tracking System on Digital Simulations for Robotic Surgical Applications</p>
+          </div>
+
+          <div className="timeline-item">
+            <div className="experience-title">LDCU Robotics Exhibit (2025)</div>
+            <div className="muted experience-role">Co-Lead Developer</div>
+            <p className="muted experience-description">Built a hand‑tracking system using MediaPipe to control a robotic arm. Integrated Python, ESP32, and servo systems for real‑time operation.</p>
+          </div>
+
+          <div className="timeline-item">
+            <div className="experience-title">Tech Innovation Exhibit (2024)</div>
+            <div className="muted experience-role">Project Leader</div>
+            <p className="muted experience-description">Developed an Arduino based vacuum race car that can be driven via Bluetooth or operate automatically using an ultrasonic sensor. Designed control logic, sensor behavior, and dual mode navigation.</p>
+          </div>
+
+          <div className="timeline-item">
+            <div className="experience-title">print("Hello World") (2021)</div>
+            <div className="muted experience-role">My first program</div>
+            <p className="muted experience-description">Wrote my first line of code (Python).</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
